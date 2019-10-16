4c10158288391dd982d71bd74ffb79efb8d9e2be
David_Izosimov <David.Izosimov@gmail.com>
2019-10-16 16:35:19 +0200

    Adds to performance data controller by merging performance data params with a current_user in order to bind performance data to a specific user. Adds index action to performance_data_controller and adds it to routes, and adds index tests for performance_data_spec file.

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::PerformanceDataController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
+  def index
+    @collection = current_api_v1_user.performance_data
+    render json: { entries: @collection }
+  end
+  
   def create
-    @data = PerformanceData.new(params[performance_data_params])
+    @data = PerformanceData.new(performance_data_params.merge(user: current_api_v1_user))
 
     if @data.save
       render json: { message: 'all good' }
