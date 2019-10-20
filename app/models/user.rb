598e6859d37126a007248b1426d92caf8a458d9d
David_Izosimov <David.Izosimov@gmail.com>
2019-10-20 19:43:53 +0200

    Adds to user model to validate confirmation password and adds uid and provider on signup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,17 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
   
   has_many :performance_data, class_name: 'PerformanceData'
+
+  before_validation :set_provider
+  before_validation :set_uid
+  
+  def set_provider
+    self[:provider] = "email" if self[:provider].blank?
+  end
+
+  def set_uid
+    self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  end
+
+  validates_confirmation_of :password
 end
